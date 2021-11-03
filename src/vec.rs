04162f6e8500ae96e4d30ae9e769eea921645f7e
Miezhiko <Miezhiko@gmail.com>
2021-11-03 10:03:53 +0400

    binarity optimization
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/vec.rs
+++ src/vec.rs
@@ -26,10 +26,10 @@ impl<T: PartialOrd> StalinFind<T> for Vec<T> {
         } else {
           self.stalin(i, 0, self.len() - 1)
         }
-      } else if self[m - 1] > i {
-        self.stalin(i, l, m - 1)
+      } else if self[m - 1] < i {
+        self.stalin(i, m, r - 1)
       } else {
-        self.stalin(i, m - 1, r - 1)
+        self.stalin(i, l, m - 1)
       }
     }
   }
@@ -87,11 +87,11 @@ mod tests {
     let mut unsorted = vec![33, 55, 3, 4, 7657, 6, 7, 8];
     assert_eq!(
       unsorted.stalin_find(3),
-      Some(2),
+      Some(0),
     );
     assert_eq!(
       unsorted,
-      vec![33, 55, 3, 7657, 7, 8],
+      vec![3, 7657, 7, 8],
     );
     if let Some(find_7) = unsorted.stalin_find(7) {
       assert_eq!(
